e60d2203efa54506d83adce7560fecb9d255e970
Rahul <rahulsharmadl2004@gmail.com>
2026-07-15 11:05:29 +0530

    database issue resolved

--- btf-backend/server/config/db.js
+++ btf-backend/server/config/db.js
@@ -4,8 +4,8 @@ const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/gymBackend')
         .then(() => {
             console.log('DB connection successful')
-            console.log("Connected DB:", mongoose.connection.name);
-            console.log("DB URI:", process.env.MONGO_URI);
+            // console.log("Connected DB:", mongoose.connection.name);
+            // console.log("DB URI:", process.env.MONGO_URI);
         })
             
         .catch(err => {
