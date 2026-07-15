4e419753a41815e286e2474d64725fad1f26de94
Rahul <rahulsharmadl2004@gmail.com>
2026-07-15 10:44:19 +0530

    debug database issue

--- btf-backend/server/config/db.js
+++ btf-backend/server/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/gymBackend')
-        .then(() => console.log('DB connection successful'))
+        .then(() => {
+            console.log('DB connection successful')
+            console.log("Connected DB:", mongoose.connection.name);
+        })
+            
         .catch(err => {
             console.log(err)
         })
